3ef81c53ecd485bc3c262eaf9c85626eb0ad1543
iivanic <igor_ivanic@hotmnail.com>
2022-03-01 13:11:38 +0100

    ..

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,29 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using ConsoleApp;
-using PdfSharpCore.Drawing;
-using PdfSharpCore.Pdf;
-using PdfSharpCore.Pdf.IO;
 using System.Diagnostics;
+using ConsoleApp;
 
-Console.WriteLine("Generiram PDF417 2D BAR-KOD prema HUB3a standardu.");
-
+Console.WriteLine ("Generiram PDF417 2D BAR-KOD prema HUB3a standardu.");
 
 string text =
-"HRVHUB30\nHRK\n000000000012355\nŽELJKO SENEKOVIĆ\nIVANEČKA ULICA 125\n42000 VARAŽDIN\n2DBK d.d.\nALKARSKI PROLAZ 13B\n21230 SINJ\nHR1210010051863000160\nHR01\n7269-68949637676-00019\nCOST\nTroškovi za 1. mjesec\n";
+    "HRVHUB30\nHRK\n000000000012355\nŽELJKO SENEKOVIĆ\nIVANEČKA ULICA 125\n42000 VARAŽDIN\n2DBK d.d.\nALKARSKI PROLAZ 13B\n21230 SINJ\nHR1210010051863000160\nHR01\n7269-68949637676-00019\nCOST\nTroškovi za 1. mjesec\n";
 
 Hub3a hub = new Hub3a();
 
 string barCode = "test.pdf";
-hub.DajPDFUplatnicu(barCode);
+hub.DajPDFUplatnicu (barCode);
 using
-    (var process = Process.Start(
-    new ProcessStartInfo
-    {
-        FileName = barCode
-        ,UseShellExecute = true
-    })
-    )
-{
-   
-}
+    (var process = Process.Start (
+        new ProcessStartInfo {
+            FileName = barCode, UseShellExecute = true
+        })) {
 
+    }
