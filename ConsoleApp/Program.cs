9b0d1a32579de73a090d2b9907d64caf166d8fee
Igor <igor_ivanic@hotmail.com>
2022-06-02 18:27:35 +0200

    ..

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,17 +6,17 @@
 
 using System.Diagnostics;
 
-Console.WriteLine ("Generiram PDF417 2D BAR-KOD prema HUB3a standardu.");
+Console.WriteLine("Generiram PDF417 2D BAR-KOD prema HUB3a standardu.");
 
 //if you have prepared hub3a record
 string text =
     "HRVHUB30\nHRK\n000000000012355\nŽELJKO SENEKOVIĆ\nIVANEČKA ULICA 125\n42000 VARAŽDIN\n2DBK d.d.\nALKARSKI PROLAZ 13B\n21230 SINJ\nHR1210010051863000160\nHR01\n7269-68949637676-00019\nCOST\nTroškovi za 1. mjesec\n";
-Hub3a.Hub3a hub = new Hub3a.Hub3a (text);
+Hub3a.Hub3a hub = new Hub3a.Hub3a(text);
 
 //or set properties
-hub = new Hub3a.Hub3a ();
-hub.Iznos = (123.55 * 100).ToString ();
-hub.Opis="Troškovi za 1. mjesec.";
+hub = new Hub3a.Hub3a();
+hub.Iznos = (123.55 * 100).ToString();
+hub.Opis = "Troškovi za 1. mjesec.";
 hub.PlatiteljPrvaLinija = "ŽELJKO SENEKOVIĆ";
 hub.PlatiteljDrugaLinija = "IVANEČKA ULICA 125";
 hub.PlatiteljTrecaLinija = "42000 VARAŽDIN";
@@ -29,36 +29,50 @@ hub.Model = "HR01";
 
 // You can generate file on disk
 string fileName = "test.svg";
-hub.DajBarKodSVG (fileName);
-openFile (fileName);
+hub.DajBarKodSVG(fileName);
+openFile(fileName);
 
-fileName = "test.png";
-hub.DajBarKodPNG (fileName);
-openFile (fileName);
+fileName = "test.png1";
+hub.DajBarKodPNG(fileName);
+openFile(fileName);
 
 fileName = "test.pdf";
-hub.DajPDFUplatnicu (fileName);
-openFile (fileName);
+hub.DajPDFUplatnicu(fileName);
+openFile(fileName);
 
 // or use Stream to get file
-Stream pdfStream = hub.DajPDFUplatnicu();
-// do something with Stream...
+using (Stream pdfStream = hub.DajPDFUplatnicu())
+{
+    // do somesthing with Stream...
+}
 
-Stream pngStream = hub.DajBarKodPNG();
-// do somesthing with Stream...
+
+using (Stream pngStream = hub.DajBarKodPNG())
+{
+    // do somesthing with Stream...
+}
 
 //SVG returns string, not stream
 string svgString = hub.DajBarKodSVG();
 // do something with string...
 
+
 //helper func to show file in defualt app
-static void openFile (string fileName) {
-    using
-        (var process = Process.Start (
-            new ProcessStartInfo {
-                FileName = fileName,
-                UseShellExecute = true
-            })) {
-                //
-        }
+static void openFile(string filename)
+{
+    Console.WriteLine($"Pritisnite bilo koju tipku za pokušaj otvaranja datoteke {filename}{System.Environment.NewLine}..." );
+    Console.ReadKey(true);
+        using
+            (var process = Process.Start(
+                new ProcessStartInfo
+                {
+                  //  FileName = "start",
+                    FileName = filename,
+                    UseShellExecute=true
+                }))
+        {
+            if(process==null)
+            Console.WriteLine(
+                $"Datoteka nije odmah otvorena. Možda nemate aplikaciju za otvaranje ove vrste datoteke ({System.IO.Path.GetExtension(filename)}).");
+        }  
 }
